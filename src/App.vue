1ade1bd712e3c3702126df46d3bfb040a6b4520e
m.larroze <m.larroze@asa-informatique.fr>
2021-05-28 11:05:00 +0200

    VUEJS app working

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6"
+      crossorigin="anonymous"
+    />
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+
+.asc:after {
+  content: "\25B2";
+}
+
+.desc:after {
+  content: "\25BC";
+}
+
+.highlight {
+  font-weight: bold;
+  color: red;
+  background-color: yellow;
+}
+</style>
---
+++ src/components/Listview.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <select id="value" :value="value" @change="changeVal" class="form-select" name="value">
+      <option v-for="item in options" :key="item.message">
+        {{ item }}
+      </option>
+    </select>
+  </div>
+</template>
+<script>
+export default {
+  name: "Listview",
+  props: {
+    value: String,
+    options: Array
+  },
+  methods: {
+    changeVal(event) {
+      this.$emit("input", event.target.value);
+    }
+  }
+};
+</script>
